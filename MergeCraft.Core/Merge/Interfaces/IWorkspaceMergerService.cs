0e195a927bbf7fa8bc4e75976b79e22f28c95326
Nick Pateman <nick@npsoftware.co.uk>
2024-11-03 10:30:16 +0000

    chore: started addomg workspace merger service back in

---
+++ MergeCraft.Core/Merge/Interfaces/IWorkspaceMergerService.cs
@@ -0,0 +1,10 @@
+﻿namespace MergeCraft.Core.Merge.Interfaces
+{
+    public interface IWorkspaceMergerService<T> where T : class, IComponent<T>
+    {
+        public IWorkspaceItem<T> Merge(
+            IWorkspaceItem<T> source,
+            IWorkspaceItem<T> target,
+            IComponentBom<T> bom);
+    }
+}
---
+++ MergeCraft.Core/Merge/WorkspaceMergerService.cs
@@ -0,0 +1,16 @@
+﻿using MergeCraft.Core.Merge.Interfaces;
+using System;
+
+namespace MergeCraft.Core.Merge
+{
+    public class WorkspaceMergerService : IWorkspaceMergerService<Component>
+    {
+        public IWorkspaceItem<Component> Merge(
+            IWorkspaceItem<Component> source,
+            IWorkspaceItem<Component> target,
+            IComponentBom<Component> bom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
